161185763a4e74368cfecaff5ab6c639c857fa04
zheng0xd <davidzheng@g.harvard.edu>
2019-11-27 09:09:40 -0500

    fixed sinh bug and added input validation

--- autodiffpy/reverse.py
+++ autodiffpy/reverse.py
@@ -1,6 +1,8 @@
 import numpy as np
 import math
 
+
+# class for automatic differentiation reverse mode
 class Reverse:
     def __init__(self, val):
         self.value = val
@@ -9,8 +11,7 @@ class Reverse:
 
     def get_gradient(self):
         if self.gradient_value is None:
-            self.gradient_value = sum(weight * var.get_gradient()
-                                      for weight, var in self.children)
+            self.gradient_value = sum(weight * child.get_gradient() for weight, child in self.children)
         return self.gradient_value
 
     def __add__(self, other):
@@ -80,18 +81,19 @@ class Reverse:
     def __neg__(self):
         return self.__mul__(-1)
 
+
 def sin(x):
     try:
-        z = Reverse(np.sin(x.value))
-        x.children.append((np.cos(x.value), z))
+        z = Reverse(sin(x.value))
+        x.children.append((cos(x.value), z))
         return z
     except AttributeError:
         return np.sin(x)
 
 def cos(x):
     try:
-        z = Reverse(np.cos(x.value))
-        x.children.append((-1*np.sin(x.value), z))
+        z = Reverse(cos(x.value))
+        x.children.append((-1*sin(x.value), z))
         return z
     except AttributeError:
         return np.cos(x)
@@ -100,48 +102,64 @@ def tan(x):
     return sin(x)/cos(x)
 
 def sec(x):
-    return 1 / cos(x)
+    return 1/cos(x)
 
 def csc(x):
-    return 1 / sin(x)
+    return 1/sin(x)
 
 def cot(x):
-    return 1 / tan(x)
+    return 1/tan(x)
 
 def exp(x):
     try:
-        z = Reverse(np.exp(x.value))
-        x.children.append((np.exp(x.value), z))
+        z = Reverse(exp(x.value))
+        x.children.append((exp(x.value), z))
         return z
     except AttributeError:
         return np.exp(x)
 
 def sinh(x):
-    return (exp(x)-exp(-x))/2
+    try:
+        z = Reverse(sinh(x.value))
+        x.children.append((cosh(x.value), z))
+        return z
+    except AttributeError:
+        return (exp(x)-exp(-x))/2
 
 def cosh(x):
-    return (exp(x)+exp(-x))/2
+    try:
+        z = Reverse(cosh(x.value))
+        x.children.append((sinh(x.value), z))
+        return z
+    except AttributeError:
+        return (exp(x)+exp(-x))/2
 
 def tanh(x):
     return sinh(x)/cosh(x)
 
 def sech(x):
-    return 1 / cosh(x)
+    return 1/cosh(x)
 
 def csch(x):
-    return 1 / sinh(x)
+    return 1/sinh(x)
 
 def coth(x):
-    return 1 / tanh(x)
+    return 1/tanh(x)
 
 def ln(x):
     return log(x)
 
+def log2(x):
+    return log(x, 2)
+
+def log10(x):
+    return log(x, 10)
+
 def log(x, base=np.exp(1)):
     # default log is natural log
     try:
-        z = Reverse(math.log(x.value, base))
-        x.children.append((1/(np.log(base)*x.value), z))
+        z = Reverse(log(x.value, base))
+        x.children.append((1/(log(base)*x.value), z))
         return z
     except AttributeError:
         return math.log(x, base)
@@ -151,28 +169,41 @@ def sqrt(x):
 
 
 def create_function():
-    # expression to be evaluated
-    var_list = input("Enter the variable names separated by a space:")
-    var_list = list(var_list.split(' '))
+    # declare variables that will be used in expression
+    while True:
+        try:
+            var_list = input("Enter the variable names separated by a space:")
+            var_list = list(var_list.split(' '))
+            for i in var_list:
+                assert i[0].isalpha()
+            break
+        except AssertionError:
+            print(f'INVALID INPUT: {var_list}\nAll variable names must be alphanumeric.')
 
-    # variable used in expression
-    for i in var_list:
-        val = float(input(f'Enter the value of {i}:'))
-        vars()[i] = Reverse(val)
+    # set value for declared variables
+    counter = 0
+    while counter < len(var_list):
+        var_name = var_list[counter]
+        val = input(f'Enter the value of {var_name}:')
+        try:
+            vars()[var_name] = Reverse(float(val))
+            counter += 1
+        except ValueError:
+            print(f'INVALID INPUT: {val}\nValue for {var_name} must be a real number.')
 
+    # input the expression
     expr = input("Enter the function:")
 
-    # evaluating expression
+    # evaluate the expression
     eval_expr = eval(expr)
     try:
         eval_expr.gradient_value = 1
-        print(f'expressions = {eval_expr.value}')
+        print(f'expression = {eval_expr.value}')
         for i in var_list:
             print(f'{i} gradient = {vars()[i].get_gradient()}')
-
     except AttributeError:
         print(f'There are no variables in the expression you entered: {expr}')
-        print(f'expressions = {eval_expr}')
+        print(f'expression = {eval_expr}')
 
 
 if __name__ == "__main__":
