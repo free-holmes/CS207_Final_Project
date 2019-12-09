5df3a350e59147f0969b42cbd601de010f9bba7d
Theodore Liu <theodoretliu@gmail.com>
2019-12-09 07:43:06 -0500

    Version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="autodiffpy-free-holmes",
-    version="0.0.3",
+    version="0.0.4",
     packages=["autodiffpy"],
     install_requires=["numpy"],
 )
